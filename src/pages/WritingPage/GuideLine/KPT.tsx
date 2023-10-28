74231db1d6bb1288449f93dadf9c6f20d35fbd4b
choiseohyeon <csh200310@naver.com>
2023-10-28 15:04:41 +0900

    delete/guide-useeffect

--- src/pages/WritingPage/GuideLine/KPT.tsx
+++ src/pages/WritingPage/GuideLine/KPT.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Header } from "../../../components/Header";
 import WritingPageWrap from "../WritingPageWrap";
 import { useNavigate } from "react-router-dom";
